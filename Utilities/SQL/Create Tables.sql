5d4d4717e1e04692932794c745fd789ef816ef57
Brett <brett.watters@gov.bc.ca>
2022-04-26 10:18:23 -0700

    Creation and index defintions
    Creation and index definitions.

--- Utilities/SQL/Create Tables.sql
+++ Utilities/SQL/Create Tables.sql
@@ -1,1 +1,41 @@
-' Test
+/* FormTemplates */
+CREATE TABLE FormTemplates (
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Code VARCHAR(80) NOT NULL,
+  Description VARCHAR(255) NOT NULL,
+  VersionNo INTEGER NOT NULL,
+  ClientURL VARCHAR(255) NOT NULL,
+  ProviderURL VARCHAR(255) NOT NULL,
+  IsActive BOOLEAN NOT NULL,
+  FormDefinition JSONB NOT NULL,
+  DateCreated TIMESTAMP NOT NULL  
+ );
+       
+CREATE INDEX Idx_FormTemplates_Name ON FormTemplates(Name);
+
+CREATE INDEX Idx_FormTemplates_Code ON FormTemplates(Code);
+
+/* FormsCreated */
+CREATE TABLE FormsCreated (
+   Id SERIAL PRIMARY KEY,
+   FormKey VARCHAR(30) NOT NULL,
+   FormTemplateId INTEGER NOT NULL,
+   VersionNo INTEGER NOT NULL,
+   CatchmentNo INTEGER NOT NULL,
+   StoreFrontName VARCHAR(60),   
+   IsCreated BOOLEAN NOT NULL,
+   IsInICM BOOLEAN NOT NULL,   
+   IsCompleted BOOLEAN NOT NULL,
+   FormData JSONB,
+   CreatedBy VARCHAR(128),
+   DateCreated TIMESTAMP NOT NULL
+ );
+      
+CREATE INDEX Idx_FormsCreated_FormTemplateId ON FormsCreated(FormTemplateId);
+
+CREATE INDEX Idx_FormsCreated_FormKey ON FormsCreated(FormKey);
+
+CREATE INDEX Idx_FormsCreated_CatchmentNo ON FormsCreatde(CatchmentNo);
+
+CREATE INDEX Idx_FormsCreated_DateCreated ON FormsCreated(DateCreated);
