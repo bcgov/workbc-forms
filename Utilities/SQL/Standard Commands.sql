5c7b8727c88c4c93b6608de98ce4a6d5a5382f6c
Brett <brett.watters@gov.bc.ca>
2022-05-02 09:55:33 -0700

    Update Standard Commands.sql
    Adds Standard comments to the database.

--- Utilities/SQL/Standard Commands.sql
+++ Utilities/SQL/Standard Commands.sql
@@ -4,6 +4,23 @@ DELETE FROM FormsCreated WHERE (IsCompleted)
 /* Selecting curent items to display to provider. # is the catchment number (1-45) */
 SELECT * FROM FormsCreated_Listing WHERE CatchmentNo = #
 
+/* Adding a new record, dialog prompt */
+INSERT INTO FormsCreated VALUES (
+  DEFAULT,
+  'a8cc0e8c-1d63-4022-bfa1-7b99fea4edc8',
+  #, /* FormTemplateId */
+  #, /* VersionNo */
+  #, /* CatchmentNo */
+  '', /* Storefront Name */
+  false, /* IsCreated */
+  false, /* IsInICM */
+  false, /* IsCompleted */
+  null,  /* FormData */
+  '*',   /* User name */
+   current_timestamp
+);
+
+
 /* Update a record when data is sent into the API. Note: json data may have issues with ' needing to be replaced by '' characters */
 UPDATE CreatedForms
 SET 
@@ -12,3 +29,10 @@ SET
 WHERE
   FormKey = '*formkeyvalue*'
 
+/* Update created forms if checkboxes are set for IsInICM or IsCompleted */
+UPDATE CreatedForms
+SET
+  IsInICM = true|false,
+  IsCompleted = true|false
+WHERE
+  FormKey = '*formkeyvalue*'
