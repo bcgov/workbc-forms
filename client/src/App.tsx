b39c0c8a1fa789159f6530ba6368cea480ed8aed
Agam Kamran <agamkamran@gmail.com>
2022-05-06 16:33:59 -0700

    Header and Footer Concept
    Header and Footer Concept

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
-import React, { ComponentType, useMemo } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import {Admin, BooleanField, Button, Create, Datagrid, EditButton, EditGuesser, FileField, FunctionField, List, ListGuesser, ReferenceField, ReferenceInput, Resource, SelectField, SelectInput, SimpleForm, TextField, TextInput, UrlField, useResourceContext} from 'react-admin'
-import simpleRestProvider from 'ra-data-simple-rest'
+import {Admin,  EditGuesser,  ListGuesser,  Resource} from 'react-admin';
 import fakeDataProvider from 'ra-data-fakerest'
 import { FormList } from './Forms/FormsList';
 import { FormCreate } from './Forms/FormCreate';
-
+import Layout from './Layout';
+import Footer from './footer';
 
 const dataProvider = fakeDataProvider({
   my_forms: [
@@ -18,21 +15,30 @@ const dataProvider = fakeDataProvider({
   ]
 })
 
-
-
-
-
-
-
+export const lightTheme = {
+  components: {
+      MuiAppBar: {
+          styleOverrides: {
+              colorSecondary: {
+                  color: '#fff',
+                  backgroundColor: '#003366',
+                  borderBottom: '3px solid #FCBA19'
+              },
+          },
+      },     
+  },
+}
 
 function App() {
-  return (
-    <Admin
+  return (    
+    <><Admin
+      theme={lightTheme}
       dataProvider={dataProvider}
+      layout={Layout}
     >
-      <Resource name="my_forms" list={FormList} create={FormCreate}/>
-      <Resource name="forms" list={ListGuesser} edit={EditGuesser}/>
-    </Admin>
+      <Resource name="my_forms" list={FormList} create={FormCreate} />
+      <Resource name="forms" list={ListGuesser} edit={EditGuesser} />
+    </Admin><Footer /></> 
   );
 }
 
