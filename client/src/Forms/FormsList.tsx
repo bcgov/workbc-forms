4d621cbcd841e94cce907f5dafd6ca8e370934ff
Agam Kamran <agamkamran@gmail.com>
2022-06-08 16:22:17 -0700

    Right Panel and Buttton
    Right Panel and Buttton

--- client/src/Forms/FormsList.tsx
+++ client/src/Forms/FormsList.tsx
@@ -1,75 +1,117 @@
 import * as React from 'react'
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import { BooleanField, Datagrid, FunctionField, List, ReferenceField, TextField } from "react-admin"
+import { 
+    TopToolbar,
+} from 'react-admin';
 const CopyToClipboard = require('react-copy-to-clipboard')
+const ListActions = () => (
+    <TopToolbar>
+    </TopToolbar>
+);
 
 export const FormList = (props: any) => {
     const a = "b"
     return (
-        <List {...props}>
-            <Datagrid>
-                <TextField source="id" />
-                <FunctionField
-                    label="Client Name"
-                    render={(record: any) =>
-                        record.isCreated ?
+        <><Grid container>
+            <Grid item xs={7}>
+            <List {...props}>
+                <Datagrid>
+                    <TextField source="id" />
+                    <FunctionField
+                        label="Client Name"
+                        render={(record: any) => record.isCreated ?
                             `${record.firstName} ${record.lastName}`
                             :
-                            "Pending..."
-                    }
-                />
-                <ReferenceField label="Form Name" source="formTemplateId" reference="formTemplates" link={false}>
-                    <TextField source="code" />
-                </ReferenceField>
-                <ReferenceField label="Form Title" source="formTemplateId" reference="formTemplates" link={false}>
-                    <TextField source="name" />
-                </ReferenceField>
-                <FunctionField
-                    label="Status"
-                    render={(record: any) =>
-                        !record.isCreated ?
+                            "Pending..."} />
+                    <ReferenceField label="Form Name" source="formTemplateId" reference="formTemplates" link={false}>
+                        <TextField source="code" />
+                    </ReferenceField>
+                    <ReferenceField label="Form Title" source="formTemplateId" reference="formTemplates" link={false}>
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <FunctionField
+                        label="Status"
+                        render={(record: any) => !record.isCreated ?
                             `Awaiting Service Provider`
                             :
                             record.isCompleted ?
                                 `Complete`
                                 :
-                                `Awaiting Client`
-                    }
-                />
-                <BooleanField source='isInICM' label="Added to ICM" />
-                <FunctionField
-                    label="Enter Details"
-                    render={(record: any) =>
-                        !record.isCreated ?
+                                `Awaiting Client`} />
+                    <BooleanField source='isInICM' label="Added to ICM" />
+                    <FunctionField
+                        label="Enter Details"
+                        render={(record: any) => !record.isCreated ?
                             <div>
-                                <a href={`${record.providerUrl.trim()}&token=${record.key}`} target="_blank" rel='noopener noreferrer'>Fill Out Form</a>
+                                <Button
+                                    href={`${record.providerUrl.trim()}&token=${record.key}`}
+                                    target="_blank"
+                                    rel='noopener noreferrer'
+                                    variant="contained"
+                                    sx={{ textAlign: 'center', backgroundColor: '#003366' }}>Fill Out Form
+                                </Button>
                             </div>
                             :
-                            `N/A`
-                    }
-                />
-                <FunctionField
-                    label="Copy URL"
-                    render={(record: any) =>
-                        !record.isCompleted && record.isCreated ?
+                            ``} />
+                    <FunctionField
+                        label="Copy URL"
+                        render={(record: any) => !record.isCompleted && record.isCreated ?
                             <CopyToClipboard text={`${record.clientUrl.trim()}&token=${record.key}`}>
-                                <button>Copy</button>
+                                <Button
+                                    variant="contained"
+                                    sx={{ backgroundColor: '#003366' }}>Copy</Button>
                             </CopyToClipboard>
                             :
-                            `N/A`
-                    }
-                />
-                <FunctionField
-                    label="Generate PDF"
-                    render={(record: any) =>
-                        record.isCompleted ?
+                            ``} />
+                    <FunctionField
+                        label="Generate PDF"
+                        render={(record: any) => record.isCompleted ?
                             <div>
-                                <a href={`http://localhost:8000/pdf/${record.key}`} target="_blank" rel='noopener noreferrer'>Generate PDF</a>
+                                <Button
+                                    href={`http://localhost:8000/pdf/${record.key}`}
+                                    target="_blank"
+                                    rel='noopener noreferrer'
+                                    variant="contained"
+                                    sx={{ backgroundColor: '#003366' }}>Download
+                                </Button>
+
                             </div>
                             :
-                            `Not Available`
-                    }
-                />
-            </Datagrid>
-        </List>
+                            ``} />
+                </Datagrid>
+            </List>
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={3}>
+            <List actions={<ListActions/>} {...props}>
+                <Datagrid bulkActionButtons={false}>
+                    <ReferenceField label="Form Name" source="formTemplateId" reference="formTemplates" link={false}>
+                        <TextField source="code" />
+                    </ReferenceField>
+                    <ReferenceField label="Form Title" source="formTemplateId" reference="formTemplates" link={false}>
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <FunctionField
+                        render={(record: any) => record.isCompleted ?
+                            <div>
+                                <Button
+                                    href={`http://localhost:3000/#/forms/create`}
+                                    target="_blank"
+                                    rel='noopener noreferrer'
+                                    variant="contained"
+                                    sx={{ backgroundColor: '#003366' }}>Create
+                                </Button>
+
+                            </div>
+                            :
+                            ``} />
+                </Datagrid>
+            </List>
+            </Grid>
+            <Grid item xs={1}></Grid>
+
+        </Grid></>
     )
 }
